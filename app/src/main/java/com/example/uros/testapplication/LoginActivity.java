f0d153fd7f344f58b8c08930b86bfea0ab428073
Uros <urosrakic93@gmail.com>
2017-02-09 15:29:11 +0100

    Email validation

--- app/src/main/java/com/example/uros/testapplication/LoginActivity.java
+++ app/src/main/java/com/example/uros/testapplication/LoginActivity.java
@@ -2,13 +2,48 @@ package com.example.uros.testapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class LoginActivity extends AppCompatActivity {
 
+    public static final Pattern VALID_EMAIL_ADDRESS_REGEX =
+            Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+    private EditText etEmail,etPassword;
+    private Button btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         setTitle("Login");
+        etEmail = (EditText) findViewById(R.id.etEmail);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String eMail = etEmail.getText().toString();
+                if (isEmailValid(eMail)){
+                    //Here goes other work with login...
+                    Toast.makeText(getApplicationContext(), "Email is valid!", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(getApplicationContext(), "Email is not valid!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    /**
+     *This Method check if email is valid
+     */
+    public boolean isEmailValid(String eMailString){
+        Matcher matcher = VALID_EMAIL_ADDRESS_REGEX .matcher(eMailString);
+        return matcher.find();
     }
 }
